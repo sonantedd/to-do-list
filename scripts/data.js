1b5e22887d42834944ab0b431575ed66b8760fb7
Andrey Nikonenko <sonantedd@gmail.com>
2025-10-21 23:57:35 +0300

    chore: comments

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-// поле completed - задел на будущее, чтобы использовать jsonplaceholder и localStorage в итоговой домашке
+// поле completed - задел на будущее, чтобы использовать jsonplaceholder в итоговой домашке
 const todosData = [
     { title: "Купить продукты", completed: false },
     { title: "Вынести мусор", completed: false },
